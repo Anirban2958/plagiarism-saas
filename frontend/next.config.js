e902c548990a4b16b7689aa2a2d65d6d483bfd8c
Anirban2958 <Anirbandhar2958@email.com>
2025-06-30 10:44:36 +0530

    fix: update settings.json and remove yellow line

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,  // optional but recommended
+  swcMinify: true         // optional
+};
+
+module.exports = nextConfig;
